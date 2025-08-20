b27b2b6665c8fd949f7d76f1e289203fd2dffd3b
Caio Vellani <caiovellani@outlook.com>
2025-08-19 21:54:09 -0300

    refactor: add rounded-none to Card

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <Card>
+    <Card className="rounded-none">
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
         <Button size="icon" variant="outline">
